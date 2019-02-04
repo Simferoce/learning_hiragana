8d0a9a9aa1d52e1fd53f0897900d680216e156df
Simferoce <le6monstre@hotmail.com>
2019-02-04 12:09:22 -0500

    Un bon chunk

--- lib/app/home/home.dart
+++ lib/app/home/home.dart
@@ -1,15 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:tp3/app/learning/learning.dart';
+import 'package:tp3/app/tryout/tryout.dart';
 import 'package:tp3/util/strings.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return HomeState();
+  }
+}
+
+class HomeState extends State<Home> {
+  int _selectedIndex = 0;
+  final _widgets = [Learning(), Tryout()];
+
   @override
   Widget build(BuildContext context) {
     var strings = Strings.of(context);
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text(strings.title),
-        ),
-        body: Center(child: Text("Hello World")));
+      appBar: AppBar(
+        title: Text(strings.title),
+      ),
+      body: Center(child: _widgets.elementAt(_selectedIndex)),
+      bottomNavigationBar: BottomNavigationBar(items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(icon: Icon(Icons.book), title: Text("Learning")),
+        BottomNavigationBarItem(icon: Icon(Icons.create), title: Text("Tryout"))
+      ],
+      currentIndex: _selectedIndex,
+          fixedColor: Colors.red,
+      onTap: _onItemTapped),
+
+    );
   }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
 }
---
+++ lib/app/learning/learning.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:tp3/app/hiragana/hiragana.dart';
+import 'package:tp3/util/dimensions.dart';
+
+class Learning extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: CustomScrollView(
+        primary: false,
+        slivers: <Widget>[
+          SliverPadding(
+            padding: const EdgeInsets.all(20.0),
+            sliver: SliverGrid.count(
+              crossAxisSpacing: 10.0,
+              crossAxisCount: 2,
+              children: List<Widget>.generate(
+                  Hiraganas.length - 1,
+                  (i) =>
+                      Card(
+                            child: Column(children: <Widget>[
+                          Expanded(
+                            child: Padding(
+                                child: FittedBox(
+                                  child: Text(
+                                    Hiraganas.keys.elementAt(i),
+                                    textScaleFactor: FontScaleDisplay1,
+                                  ),
+                                ),
+                                padding: EdgeInsets.all(PaddingLarge)),
+                          ),
+                          Padding(
+                            child: Text(Hiraganas.values.elementAt(i)),
+                            padding: EdgeInsets.only(bottom: PaddingLarge),
+                          )
+                        ]))
+
+                      ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
