95b823bcd9d65428d6754bf14517d4a12e7a5579
Simferoce <le6monstre@hotmail.com>
2019-02-07 11:23:32 -0500

    reduce duplicate code button choice

--- lib/app/training/training.dart
+++ lib/app/training/training.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:tp3/app/training/question.dart';
 import 'package:tp3/util/dimensions.dart';
 
+final NUMBER_OF_CHOICE_BUTTONS = 3;
+
 class Training extends StatefulWidget {
   State<StatefulWidget> createState() => TrainingState();
 }
@@ -40,60 +42,38 @@ class TrainingState extends State<Training> {
         Padding(
           padding: const EdgeInsets.all(PaddingNormal),
           child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(0),
-                child: FlatButton(
-                    onPressed: question.answers[0].hasBeenAnswered
-                        ? null
-                        : () => setState(() {
-                              question.answerQuestion(onRightAnswerAnswered, 0);
-                            }),
-                    child: Text(
-                      question.answers[0].answer,
-                      style: TextStyle(
-                          color: !question.answers[0].hasBeenAnswered
-                              ? Colors.black
-                              : Colors.red),
-                    )),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(0),
-                child: FlatButton(
-                    onPressed: question.answers[1].hasBeenAnswered
-                        ? null
-                        : () => setState(() {
-                              question.answerQuestion(onRightAnswerAnswered, 1);
-                            }),
-                    child: Text(
-                      question.answers[1].answer,
-                      style: TextStyle(
-                          color: !question.answers[1].hasBeenAnswered
-                              ? Colors.black
-                              : Colors.red),
-                    )),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(0),
-                child: FlatButton(
-                  onPressed: question.answers[2].hasBeenAnswered
-                      ? null
-                      : () => setState(() {
-                            question.answerQuestion(onRightAnswerAnswered, 2);
-                          }),
-                  child: Text(
-                    question.answers[2].answer,
-                    style: TextStyle(
-                        color: !question.answers[2].hasBeenAnswered
-                            ? Colors.black
-                            : Colors.red),
-                  ),
-                ),
-              )
-            ],
+            children: buildChoiceButtons(NUMBER_OF_CHOICE_BUTTONS),
           ),
         ),
       ],
     );
+  }
+
+  List<Widget> buildChoiceButtons(int numberOfButton) {
+    List<Widget> buttons = List<Widget>();
+    for(int i = 0; i < numberOfButton; ++i) {
+      buttons.add(buildChoiceButton(i));
+    }
+
+    return buttons;
+  }
+
+  Padding buildChoiceButton(int index) {
+    return Padding(
+              padding: const EdgeInsets.all(0),
+              child: FlatButton(
+                  onPressed: question.answers[index].hasBeenAnswered
+                      ? null
+                      : () => setState(() {
+                            question.answerQuestion(onRightAnswerAnswered, index);
+                          }),
+                  child: Text(
+                    question.answers[index].answer,
+                    style: TextStyle(
+                        color: !question.answers[index].hasBeenAnswered
+                            ? Colors.black
+                            : Colors.red),
+                  )),
+            );
   }
 }
